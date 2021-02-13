2b8fbcc13f6417c9a07912da3708a5796d205bf9
Jxl-v <78710756+Jxl-v@users.noreply.github.com>
2021-02-13 13:23:30 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local baseUrl = "https://raw.githubusercontent.com/Jxl-v/schematica/main/"
 
---local function require_module(module) return loadstring(game:HttpGet(string.format("%sdependencies/%s", baseUrl, module)))() end
-local function require_module(module) return loadstring(readfile(string.format("schematica-script-workspace/dependencies/%s", module)))() end
+local function require_module(module) return loadstring(game:HttpGet(string.format("%sdependencies/%s", baseUrl, module)))() end
+--local function require_module(module) return loadstring(readfile(string.format("schematica-script-workspace/dependencies/%s", module)))() end
 
 local Http = game.HttpService
 local env = Http:JSONDecode(game:HttpGet(baseUrl .. "env.json"))
